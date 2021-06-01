b399f9a8c8062735105e595f2ad9b239873fc5e1
tiago.eem@gmail.com <tiago.eem@gmail.com>
2021-06-01 12:58:48 -0300

    apresentacao e notebook

--- 5. app-Em-Sala/app.py
+++ 5. app-Em-Sala/app.py
@@ -1,5 +1,17 @@
 from model.funcoes_simples_db import select_cliente, select_todos_clientes, delete_cliente, update_cliente, insert_cliente, teste_visualizar_tabelas, select_query
+from flask import Flask, request
 
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/cliente/<int:id>", methods=['GET'])
+def consulta_cliente(id):
+    return select_cliente(id) , 200
 
 # Fun;áo main para teste dos exemplos
 def main():
@@ -20,4 +32,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
